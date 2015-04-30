1c2c335963ff15eeb5787209e9a8b6fd61dbba70
Oscunach <Oscunach@Oscunach-PC>
2015-04-30 18:27:18 -0500

    30-04-2015 ---11:20

--- src/java/vif/logica/funciones/FDenuncia.java
+++ src/java/vif/logica/funciones/FDenuncia.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package vif.logica.funciones;
+
 import accesodatos.ConjuntoResultado;
 import java.util.ArrayList;
 import accesodatos.AccesoDatos;
@@ -21,23 +22,22 @@ import vif.logica.funciones.FSubcircuito;
 import vif.logica.funciones.FVictima;
 import vif.logica.funciones.FViolencia;
 
-
 /**
  *
  * @author Oscunach
  */
 public class FDenuncia {
-    
+
     //llenar datos
     public static ArrayList<Denuncia> llenarDatos(ConjuntoResultado rs) throws Exception {
         ArrayList<Denuncia> lst = new ArrayList<Denuncia>();
         Denuncia denuncia = null;
         try {
             while (rs.next()) {
-                denuncia = new Denuncia(rs.getInt("pid_denuncia"),rs.getDate("pfecha_denuncia"),rs.getString("pdenunciante"),FVictima.ObtenerVictimaDadoId(rs.getInt("pid_victima")),rs.getDouble("px"),rs.getDouble("py"),
-                                        FCircuito.ObtenerCircuitoDadoId(rs.getInt("pid_circuito")),FSubcircuito.ObtenerSubcircuitoDadoId(rs.getInt("pid_subcircuito")),FAgresor.ObtenerAgresorDadoId(rs.getInt("pid_agresor")),
-                                        rs.getString("prelacion_victima_agresor"),rs.getInt("pnumero_hijos"),FViolencia.ObtenerViolenciaDadoId(rs.getInt("pid_tipo_violencia")),rs.getDate("pfecha_agresion"),
-                                        rs.getTime("phora_agresion"),rs.getString("pmedidas_amparo"),rs.getString("psentencia"),rs.getString("papelacion"));
+                denuncia = new Denuncia(rs.getInt("pid_denuncia"), rs.getDate("pfecha_denuncia"), rs.getString("pdenunciante"), FVictima.ObtenerVictimaDadoId(rs.getInt("pid_victima")), rs.getDouble("px"), rs.getDouble("py"),
+                        FCircuito.ObtenerCircuitoDadoId(rs.getInt("pid_circuito")), FSubcircuito.ObtenerSubcircuitoDadoId(rs.getInt("pid_subcircuito")), FAgresor.ObtenerAgresorDadoId(rs.getInt("pid_agresor")),
+                        rs.getString("prelacion_victima_agresor"), rs.getInt("pnumero_hijos"), FViolencia.ObtenerViolenciaDadoId(rs.getInt("pid_tipo_violencia")), rs.getDate("pfecha_agresion"),
+                        rs.getTime("phora_agresion"), rs.getString("pmedidas_amparo"), rs.getString("psentencia"), rs.getString("papelacion"));
                 lst.add(denuncia);
             }
         } catch (Exception e) {
@@ -46,7 +46,7 @@ public class FDenuncia {
         }
         return lst;
     }
-    
+
     public static ArrayList<Denuncia> obtenerDenuncia() throws Exception {
         ArrayList<Denuncia> lst = new ArrayList<Denuncia>();
         try {
@@ -54,21 +54,68 @@ public class FDenuncia {
             ConjuntoResultado rs = AccesoDatos.ejecutaQuery(sql);
             lst = llenarDatos(rs);
             rs = null;
-            
+
         } catch (SQLException exConec) {
             throw new Exception(exConec.getMessage());
         }
-        
+
         return lst;
     }
-    
+
     public static boolean insertarDenuncia(Denuncia denuncia) throws Exception {
         boolean eje = false;
         try {
             ArrayList<Parametro> lstP = new ArrayList<Parametro>();
-            String sql = "select * from vif.f_insert_denuncia(?,?,?)";
-            lstP.add(new Parametro(1, denuncia.getFecha()));            
-            
+            String sql = "select * from vif.f_insert_denuncia(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            lstP.add(new Parametro(1, denuncia.getFecha()));
+            lstP.add(new Parametro(2, denuncia.getDenunciante()));
+            lstP.add(new Parametro(3, denuncia.getId_victima().getId_victima()));
+            lstP.add(new Parametro(4, denuncia.getX()));
+            lstP.add(new Parametro(5, denuncia.getY()));
+            lstP.add(new Parametro(6, denuncia.getId_circuito().getId_circuito()));
+            lstP.add(new Parametro(7, denuncia.getId_subcircuito().getId_subcircuito()));
+            lstP.add(new Parametro(8, denuncia.getId_agresor().getId_agresor()));
+            lstP.add(new Parametro(9, denuncia.getRelacion_victima_agresor()));
+            lstP.add(new Parametro(10, denuncia.getNumero_hijos()));
+            lstP.add(new Parametro(11, denuncia.getId_tipo_violencia().getId_violencia()));
+            lstP.add(new Parametro(12, denuncia.getFecha_agresion()));
+            lstP.add(new Parametro(13, denuncia.getHora_agresion()));
+            lstP.add(new Parametro(14, denuncia.getMedidas_amparo()));
+            lstP.add(new Parametro(15, denuncia.getSentencia()));
+            lstP.add(new Parametro(16, denuncia.getApelacion()));
+            ConjuntoResultado rs = AccesoDatos.ejecutaQuery(sql, lstP);
+            while (rs.next()) {
+                if (rs.getString(0).equals("true"));
+                eje = true;
+            }
+        } catch (SQLException exConec) {
+            throw new Exception(exConec.getMessage());
+        }
+        return eje;
+    }
+
+    public static boolean actualizarDenuncia(Denuncia denuncia) throws Exception {
+        boolean eje = false;
+        try {
+            ArrayList<Parametro> lstP = new ArrayList<Parametro>();
+            String sql = "select * from vif.f_update_denuncia(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            lstP.add(new Parametro(1, denuncia.getFecha()));
+            lstP.add(new Parametro(2, denuncia.getDenunciante()));
+            lstP.add(new Parametro(3, denuncia.getId_victima().getId_victima()));
+            lstP.add(new Parametro(4, denuncia.getX()));
+            lstP.add(new Parametro(5, denuncia.getY()));
+            lstP.add(new Parametro(6, denuncia.getId_circuito().getId_circuito()));
+            lstP.add(new Parametro(7, denuncia.getId_subcircuito().getId_subcircuito()));
+            lstP.add(new Parametro(8, denuncia.getId_agresor().getId_agresor()));
+            lstP.add(new Parametro(9, denuncia.getRelacion_victima_agresor()));
+            lstP.add(new Parametro(10, denuncia.getNumero_hijos()));
+            lstP.add(new Parametro(11, denuncia.getId_tipo_violencia().getId_violencia()));
+            lstP.add(new Parametro(12, denuncia.getFecha_agresion()));
+            lstP.add(new Parametro(13, denuncia.getHora_agresion()));
+            lstP.add(new Parametro(14, denuncia.getMedidas_amparo()));
+            lstP.add(new Parametro(15, denuncia.getSentencia()));
+            lstP.add(new Parametro(16, denuncia.getApelacion()));
+            lstP.add(new Parametro(17, denuncia.getId_denucnia()));
             ConjuntoResultado rs = AccesoDatos.ejecutaQuery(sql, lstP);
             while (rs.next()) {
                 if (rs.getString(0).equals("true"));
@@ -80,4 +127,21 @@ public class FDenuncia {
         return eje;
     }
     
+    public static boolean eliminar(int codigo) throws Exception {
+        boolean eje = false;
+        try {
+            ArrayList<Parametro> lstP = new ArrayList<Parametro>();
+            String sql = "select * from vif.f_delete_denuncia(?)";
+            lstP.add(new Parametro(1, codigo));
+            ConjuntoResultado rs = AccesoDatos.ejecutaQuery(sql, lstP);
+            while (rs.next()) {
+                if (rs.getString(0).equals("true"));
+                eje = true;
+            }
+        } catch (SQLException exConec) {
+            throw new Exception(exConec.getMessage());
+        }
+        return eje;
+    }
+
 }
---
+++ src/java/vif/presentacion/beans/DenunciaControlador.java
@@ -0,0 +1,269 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vif.presentacion.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import java.util.ArrayList;
+import org.primefaces.context.DefaultRequestContext;
+import recursos.Util;
+import vif.logica.clases.Agresor;
+import vif.logica.clases.Circuito;
+import vif.logica.clases.Subcircuito;
+import vif.logica.clases.Denuncia;
+import vif.logica.clases.Victima;
+import vif.logica.clases.Violencia;
+import vif.logica.funciones.FAgresor;
+import vif.logica.funciones.FCircuito;
+import vif.logica.funciones.FDenuncia;
+import vif.logica.funciones.FSubcircuito;
+import vif.logica.funciones.FVictima;
+import vif.logica.funciones.FViolencia;
+
+/**
+ *
+ * @author Oscunach
+ */
+@ManagedBean
+@RequestScoped
+public class DenunciaControlador {
+
+    /**
+     * Creates a new instance of DenunciaControlador
+     */
+    private Denuncia objDenuncia;
+    private Denuncia denunciaSel;
+    private ArrayList<Denuncia> lstDenuncia;
+    private ArrayList<Agresor> lstAgresor;
+    private ArrayList<Circuito> lstCircuito;
+    private ArrayList<Subcircuito> lstSubcircuito;
+    private ArrayList<Victima> lstVictima;
+    private ArrayList<Violencia> lstViolencia;
+    private boolean mostrarActualizar;
+    private int valorDSeleccionado;
+    private int valorASeleccionado;
+    private int valorCSeleccionada;
+    private int valorSubcSeleccionada;
+    private int valorVicSeleccionada;
+    private int valorViolSeleccionada;
+
+    public DenunciaControlador() {
+        this.reinit();
+    }
+
+    public Denuncia getObjDenuncia() {
+        return objDenuncia;
+    }
+
+    public void setObjDenuncia(Denuncia objDenuncia) {
+        this.objDenuncia = objDenuncia;
+    }
+
+    public Denuncia getDenunciaSel() {
+        return denunciaSel;
+    }
+
+    public void setDenunciaSel(Denuncia denunciaSel) {
+        this.denunciaSel = denunciaSel;
+    }
+
+    public ArrayList<Denuncia> getLstDenuncia() {
+        return lstDenuncia;
+    }
+
+    public void setLstDenuncia(ArrayList<Denuncia> lstDenuncia) {
+        this.lstDenuncia = lstDenuncia;
+    }
+
+    public ArrayList<Agresor> getLstAgresor() {
+        return lstAgresor;
+    }
+
+    public void setLstAgresor(ArrayList<Agresor> lstAgresor) {
+        this.lstAgresor = lstAgresor;
+    }
+
+    public ArrayList<Circuito> getLstCircuito() {
+        return lstCircuito;
+    }
+
+    public void setLstCircuito(ArrayList<Circuito> lstCircuito) {
+        this.lstCircuito = lstCircuito;
+    }
+
+    public ArrayList<Subcircuito> getLstSubcircuito() {
+        return lstSubcircuito;
+    }
+
+    public void setLstSubcircuito(ArrayList<Subcircuito> lstSubcircuito) {
+        this.lstSubcircuito = lstSubcircuito;
+    }
+
+    public ArrayList<Victima> getLstVictima() {
+        return lstVictima;
+    }
+
+    public void setLstVictima(ArrayList<Victima> lstVictima) {
+        this.lstVictima = lstVictima;
+    }
+
+    public ArrayList<Violencia> getLstViolencia() {
+        return lstViolencia;
+    }
+
+    public void setLstViolencia(ArrayList<Violencia> lstViolencia) {
+        this.lstViolencia = lstViolencia;
+    }
+
+    public boolean isMostrarActualizar() {
+        return mostrarActualizar;
+    }
+
+    public void setMostrarActualizar(boolean mostrarActualizar) {
+        this.mostrarActualizar = mostrarActualizar;
+    }
+
+    public int getValorDSeleccionado() {
+        return valorDSeleccionado;
+    }
+
+    public void setValorDSeleccionado(int valorDSeleccionado) {
+        this.valorDSeleccionado = valorDSeleccionado;
+    }
+
+    public int getValorASeleccionado() {
+        return valorASeleccionado;
+    }
+
+    public void setValorASeleccionado(int valorASeleccionado) {
+        this.valorASeleccionado = valorASeleccionado;
+    }
+
+    public int getValorCSeleccionada() {
+        return valorCSeleccionada;
+    }
+
+    public void setValorCSeleccionada(int valorCSeleccionada) {
+        this.valorCSeleccionada = valorCSeleccionada;
+    }
+
+    public int getValorSubcSeleccionada() {
+        return valorSubcSeleccionada;
+    }
+
+    public void setValorSubcSeleccionada(int valorSubcSeleccionada) {
+        this.valorSubcSeleccionada = valorSubcSeleccionada;
+    }
+
+    public int getValorVicSeleccionada() {
+        return valorVicSeleccionada;
+    }
+
+    public void setValorVicSeleccionada(int valorVicSeleccionada) {
+        this.valorVicSeleccionada = valorVicSeleccionada;
+    }
+
+    public int getValorViolSeleccionada() {
+        return valorViolSeleccionada;
+    }
+
+    public void setValorViolSeleccionada(int valorViolSeleccionada) {
+        this.valorViolSeleccionada = valorViolSeleccionada;
+    }
+
+    private void reinit() {
+
+    }
+
+    public void cargarDenuncia() {
+        try {
+            this.lstDenuncia = FDenuncia.obtenerDenuncia();
+            this.denunciaSel = lstDenuncia.get(0);
+            System.out.println(lstDenuncia.get(0).getId_denucnia());
+        } catch (Exception e) {
+            Util.addErrorMessage("public void cargarDenuncia() dice: " + e.getMessage());
+            System.out.println("public void cargarDenuncia() dice: " + e.getMessage());
+        }
+    }
+
+    public void cargarAgresor() {
+        try {
+            this.lstAgresor = FAgresor.ObtenerAgresor();
+            System.out.println(lstAgresor.get(0).getId_agresor());
+        } catch (Exception e) {
+            Util.addErrorMessage("public void cargarAgresor dice: " + e.getMessage());
+            System.out.println("public void cargarAgresor dice: " + e.getMessage());
+        }
+    }
+
+    public void cargarVictima() {
+        try {
+            this.lstVictima = FVictima.ObtenerVictimas();
+            System.out.println(lstVictima.get(0).getId_victima());
+        } catch (Exception e) {
+            Util.addErrorMessage(" public void cargarVictima dice:" + e.getMessage());
+            System.out.println(" public void cargarVictima dice:" + e.getMessage());
+        }
+    }
+
+    public void cargarCircuito() {
+        try {
+            this.lstCircuito = FCircuito.obtenerCircuito();
+            System.out.println(lstCircuito.get(0).getId_circuito());
+        } catch (Exception e) {
+            Util.addErrorMessage(" public void cargarCircuito dice:" + e.getMessage());
+            System.out.println(" public void cargarCircuito dice:" + e.getMessage());
+        }
+    }
+
+    public void cargarSubcircuito() {
+        try {
+            this.lstSubcircuito = FSubcircuito.obtenerSubcircuito();
+            System.out.println(lstSubcircuito.get(0).getId_subcircuito());
+        } catch (Exception e) {
+            Util.addErrorMessage("public void cargarSubcircuito dice:" + e.getMessage());
+            System.out.println("public void cargarSubcircuito dice: " + e.getMessage());
+        }
+    }
+
+    public void cargarViolencia() {
+        try {
+            this.lstViolencia = FViolencia.obtenerViolencia();
+            System.out.println(lstViolencia.get(0).getId_violencia());
+        } catch (Exception e) {
+            Util.addErrorMessage("public void cargarViolencia dice :" + e.getMessage());
+            System.out.println("public void cargarViolenciao dice: " + e.getMessage());
+        }
+    }
+    
+     public void insertarDenuncia() {
+        try {            
+            objDenuncia.setId_victima(FVictima.ObtenerVictimaDadoId(valorVicSeleccionada));
+            objDenuncia.setId_circuito(FCircuito.ObtenerCircuitoDadoId(valorCSeleccionada));
+            objDenuncia.setId_subcircuito(FSubcircuito.ObtenerSubcircuitoDadoId(valorSubcSeleccionada));
+            objDenuncia.setId_agresor(FAgresor.ObtenerAgresorDadoId(valorASeleccionado));
+            objDenuncia.setId_tipo_violencia(FViolencia.ObtenerViolenciaDadoId(valorViolSeleccionada));
+            if(FDenuncia.insertarDenuncia(objDenuncia)){
+                this.reinit();
+                DefaultRequestContext.getCurrentInstance().execute("wdlgNuevaDenuncia.hide()");
+                Util.addSuccessMessage("Información Guardada con éxito");
+                System.out.println("public void insertarDenuncia dice: Información Guardada con éxito");
+
+            } else {
+                Util.addSuccessMessage("Error al guardar la información");
+                System.out.println("public void insertarDenuncia dice: Error al guardar la información");
+            }
+        } catch (Exception e) {
+            Util.addErrorMessage("public void insertarDenuncia dice: " + e.getMessage());
+            System.out.println("public void insertarDenuncia dice: " + e.getMessage());
+        }
+    }
+
+    public void cambiarEstadoMostrarActualizar() {
+        mostrarActualizar = true;
+    }
+    
+}
