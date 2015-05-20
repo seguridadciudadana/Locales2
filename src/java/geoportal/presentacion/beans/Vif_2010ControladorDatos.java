93ab43c76c52baabcecf6de275bb2e16c4344b61
djdreams <edison_pol@hotmail.com>
2015-05-20 11:11:23 -0500

    20/05/2015

---
+++ src/java/geoportal/presentacion/beans/Vif_2010ControladorDatos.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geoportal.presentacion.beans;
+
+import geoportal.logica.clases.Vif_2010;
+import geoportal.logica.funciones.FVif_2010;
+import java.util.ArrayList;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import recursos.Util;
+
+/**
+ *
+ * @author servidor
+ */
+@ManagedBean
+@RequestScoped
+public class Vif_2010ControladorDatos {
+    private ArrayList<Vif_2010> lstDatos;
+    private Vif_2010 datoSel;
+
+    public Vif_2010 getDatoSel() {
+        return datoSel;
+    }
+
+    public void setDatoSel(Vif_2010 datoSel) {
+        this.datoSel = datoSel;
+    }
+
+    
+    public ArrayList<Vif_2010> getLstDatos() {
+        return lstDatos;
+    }
+
+    public void setLstDatos(ArrayList<Vif_2010> lstDatos) {
+        this.lstDatos = lstDatos;
+    }
+    
+    public Vif_2010ControladorDatos() {
+        this.reinit();
+    }
+
+    private void reinit() {
+        
+        this.lstDatos = new ArrayList<Vif_2010>();
+        
+        this.cargarDatos();
+        
+    }
+    
+    public void cargarDatos() {
+        try {
+
+            this.lstDatos = FVif_2010.ObtenerDatos();
+            this.datoSel = lstDatos.get(0);
+            System.out.println(lstDatos.get(0).getId());
+
+        } catch (Exception e) {
+            Util.addErrorMessage("private void cargarDatos dice: " + e.getMessage());
+            System.out.println("private void cargarDatos dice: " + e.getMessage());
+        }
+    }
+}
