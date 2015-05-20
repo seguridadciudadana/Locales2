2a2447891fc865d8b8cf27eb7e2d4b214982e5ff
Oscunach <Oscunach@Oscunach-PC>
2015-05-20 11:16:26 -0500

    20-05-2015 --Geovanny

--- src/java/geoportal/logica/funciones/FVif_2011.java
+++ src/java/geoportal/logica/funciones/FVif_2011.java
@@ -46,19 +46,19 @@ public class FVif_2011 {
                         rs.getString("pdomiciliado_agresor"),
                         rs.getString("pestado_civil_agresor"),
                         rs.getString("pnivel_de_instruccion_agresor"),
-                        rs.getString("pocupacion_agresor"), 
-                        rs.getString("pparentesco_victima_agresor"), 
-                        rs.getString("pnumeros_hijos_comun"), 
-                        rs.getString("plugar_agresion"), 
+                        rs.getString("pocupacion_agresor"),
+                        rs.getString("pparentesco_victima_agresor"),
+                        rs.getString("pnumeros_hijos_comun"),
+                        rs.getString("plugar_agresion"),
                         rs.getString("ptipo_de_violencia"),
-                        rs.getDate("pfecha_agresion"), 
-                        rs.getString("phora_de_agresion"), 
-                        rs.getString("pmedidas_de_amparo"), 
-                        rs.getString("psentencia"), 
-                        rs.getString("papelacion"), 
-                        rs.getString("pboletas_anteriores"), 
+                        rs.getDate("pfecha_agresion"),
+                        rs.getString("phora_de_agresion"),
+                        rs.getString("pmedidas_de_amparo"),
+                        rs.getString("psentencia"),
+                        rs.getString("papelacion"),
+                        rs.getString("pboletas_anteriores"),
                         rs.getString("pobservaciones"),
-                        rs.getString("pboletas_de_remision"), 
+                        rs.getString("pboletas_de_remision"),
                         rs.getInt("pid")
                 );
 
@@ -70,11 +70,11 @@ public class FVif_2011 {
         }
         return lst;
     }
-    
+
     public static ArrayList<Vif_2011> ObtenerDatos() throws Exception {
         ArrayList<Vif_2011> lst = new ArrayList<Vif_2011>();
         try {
-            String sql = "select * from geoportal.f_select_vif2011()";
+            String sql = "select * from geoportal.f_select_vif2011_2()";
             ConjuntoResultado rs = AccesoDatos.ejecutaQuery(sql);
             lst = llenarDatos(rs);
             rs = null;
@@ -83,7 +83,5 @@ public class FVif_2011 {
         }
         return lst;
     }
-    
-    
 
 }
---
+++ src/java/geoportal/presentacion/beans/Vif_2011ControladorDatos.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geoportal.presentacion.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import geoportal.logica.clases.Vif_2011;
+import geoportal.logica.funciones.FVif_2011;
+import java.io.Serializable;
+//import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import org.primefaces.context.DefaultRequestContext;
+//import org.primefaces.model.chart.Axis;
+//import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.CartesianChartModel;
+//import org.primefaces.model.chart.CategoryAxis;
+import org.primefaces.model.chart.ChartSeries;
+//import org.primefaces.model.chart.LineChartModel;
+//import org.primefaces.model.chart.PieChartModel;
+import recursos.Util;
+//import vif.logica.clases.subcircuito;
+
+/**
+ *
+ * @author Oscunach
+ */
+@ManagedBean
+@RequestScoped
+public class Vif_2011ControladorDatos {
+
+    /**
+     * Creates a new instance of Vif_2011ControladorDatos
+     */
+    private Vif_2011 objVif_2011;
+    private Vif_2011 objVif_2011Sel;
+    private ArrayList<Vif_2011> lstVif_2011;
+    private boolean mostrarActualizar;
+
+    public Vif_2011 getObjVif_2011() {
+        return objVif_2011;
+    }
+
+    public void setObjVif_2011(Vif_2011 objVif_2011) {
+        this.objVif_2011 = objVif_2011;
+    }
+
+    public Vif_2011 getObjVif_2011Sel() {
+        return objVif_2011Sel;
+    }
+
+    public void setObjVif_2011Sel(Vif_2011 objVif_2011Sel) {
+        this.objVif_2011Sel = objVif_2011Sel;
+    }
+
+    public ArrayList<Vif_2011> getLstVif_2011() {
+        return lstVif_2011;
+    }
+
+    public void setLstVif_2011(ArrayList<Vif_2011> lstVif_2011) {
+        this.lstVif_2011 = lstVif_2011;
+    }
+
+    public boolean isMostrarActualizar() {
+        return mostrarActualizar;
+    }
+
+    public void setMostrarActualizar(boolean mostrarActualizar) {
+        this.mostrarActualizar = mostrarActualizar;
+    }
+
+    public Vif_2011ControladorDatos() {
+        this.reinit();
+    }
+
+    public void reinit() {
+        this.objVif_2011 = new Vif_2011();
+        this.objVif_2011Sel = new Vif_2011();
+        this.lstVif_2011 = new ArrayList<Vif_2011>();
+        this.obtenerDatosVif_2011();
+    }
+    
+    public void obtenerDatosVif_2011() {
+        try {
+            this.lstVif_2011=FVif_2011.ObtenerDatos();
+            this.objVif_2011Sel=lstVif_2011.get(0);
+            System.out.println(lstVif_2011.get(0).getId());
+        } catch (Exception e) {
+            Util.addErrorMessage("public void obtenerDatosVif2011 dice: "+e.getMessage());
+            System.out.println("public void obtenerDatosVif2011 dice: "+e.getMessage());
+        }
+
+    }
+
+}
