b3e19e1a999bd996a1a9daeaf097b23f02109c2b
oscunach <oscunach@oscunach-PC>
2015-06-17 10:37:22 -0500

    sala 1ra acogida

---
+++ src/java/geoportal/logica/clases/Vif_2012_Sala_1acogida.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geoportal.logica.clases;
+
+/**
+ *
+ * @author oscunach
+ */
+public class Vif_2012_Sala_1acogida {
+    
+    
+}
