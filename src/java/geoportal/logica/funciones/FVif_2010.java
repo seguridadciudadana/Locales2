c2b983ddc80038b49a729250db0010ea3657bcf4
Oscunach <Oscunach@Oscunach-PC>
2015-07-15 12:16:19 -0500

    parentesco_2011

--- src/java/geoportal/logica/funciones/FVif_2010.java
+++ src/java/geoportal/logica/funciones/FVif_2010.java
@@ -1363,4 +1363,24 @@ public class FVif_2010 {
         }
         return lst;
     }
+    
+    public static ArrayList<Vif_2010> ObtenerDatosDadoDiaAgresionSexoVictima(String circuito, String sexo) throws Exception {
+        ArrayList<Vif_2010> lst = new ArrayList<Vif_2010>();
+        try {
+            ArrayList<Parametro> lstP = new ArrayList<Parametro>();
+            String sql = "select * from geoportal.f_select_vif_2010_2_dado_dia_agresion_sexo_victima(?,?)";
+            lstP.add(new Parametro(1, circuito));
+            lstP.add(new Parametro(2, sexo));
+            ConjuntoResultado rs = AccesoDatos.ejecutaQuery(sql, lstP);
+
+            lst = llenarDatos(rs);
+            rs = null;
+        } catch (SQLException exConec) {
+            throw new Exception(exConec.getMessage());
+        }
+        return lst;
+    }
+    
+    
+    
 }
