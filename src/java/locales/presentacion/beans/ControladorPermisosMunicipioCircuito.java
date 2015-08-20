76b4b7a0fa16e3123d25158d8dc9bdb6dd6e8460
CMoreno <>
2015-08-20 09:04:04 -0500

    20/08/2015 //Controladores - Vistas  // CMoreno

---
+++ src/java/locales/presentacion/beans/ControladorPermisosMunicipioCircuito.java
@@ -0,0 +1,149 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package locales.presentacion.beans;
+
+import java.util.ArrayList;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import locales.logica.clases.Locales;
+import locales.logica.funciones.FLocales;
+import org.primefaces.model.chart.CartesianChartModel;
+import org.primefaces.model.chart.ChartSeries;
+import recursos.Util;
+
+/**
+ *
+ * @author oscunach
+ */
+@ManagedBean
+@RequestScoped
+public class ControladorPermisosMunicipioCircuito {
+
+    /**
+     * Creates a new instance of ControladorPermisosMunicipioCircuito
+     */
+    private CartesianChartModel lineLocalesCircuitos;
+    private ArrayList<Locales> lstLocales;
+    private ArrayList<Locales> lstLocalesDadoCircuito;
+    private ArrayList<Locales> lstLocalesCircuito;
+    private ArrayList<Locales> lstLocalesDadoCircuitoSi;
+    private ArrayList<Locales> lstLocalesDadoCircuitoNo;
+    private ArrayList<Locales> lstLocalesDadoCircuitoNoPresenta;
+
+    public CartesianChartModel getLineLocalesCircuitos() {
+        return lineLocalesCircuitos;
+    }
+
+    public void setLineLocalesCircuitos(CartesianChartModel lineLocalesCircuitos) {
+        this.lineLocalesCircuitos = lineLocalesCircuitos;
+    }
+
+    public ArrayList<Locales> getLstLocales() {
+        return lstLocales;
+    }
+
+    public void setLstLocales(ArrayList<Locales> lstLocales) {
+        this.lstLocales = lstLocales;
+    }
+
+    public ArrayList<Locales> getLstLocalesDadoCircuito() {
+        return lstLocalesDadoCircuito;
+    }
+
+    public void setLstLocalesDadoCircuito(ArrayList<Locales> lstLocalesDadoCircuito) {
+        this.lstLocalesDadoCircuito = lstLocalesDadoCircuito;
+    }
+
+    public ArrayList<Locales> getLstLocalesCircuito() {
+        return lstLocalesCircuito;
+    }
+
+    public void setLstLocalesCircuito(ArrayList<Locales> lstLocalesCircuito) {
+        this.lstLocalesCircuito = lstLocalesCircuito;
+    }
+
+    public ArrayList<Locales> getLstLocalesDadoCircuitoSi() {
+        return lstLocalesDadoCircuitoSi;
+    }
+
+    public void setLstLocalesDadoCircuitoSi(ArrayList<Locales> lstLocalesDadoCircuitoSi) {
+        this.lstLocalesDadoCircuitoSi = lstLocalesDadoCircuitoSi;
+    }
+
+    public ArrayList<Locales> getLstLocalesDadoCircuitoNo() {
+        return lstLocalesDadoCircuitoNo;
+    }
+
+    public void setLstLocalesDadoCircuitoNo(ArrayList<Locales> lstLocalesDadoCircuitoNo) {
+        this.lstLocalesDadoCircuitoNo = lstLocalesDadoCircuitoNo;
+    }
+
+    public ArrayList<Locales> getLstLocalesDadoCircuitoNoPresenta() {
+        return lstLocalesDadoCircuitoNoPresenta;
+    }
+
+    public void setLstLocalesDadoCircuitoNoPresenta(ArrayList<Locales> lstLocalesDadoCircuitoNoPresenta) {
+        this.lstLocalesDadoCircuitoNoPresenta = lstLocalesDadoCircuitoNoPresenta;
+    }
+
+    @PostConstruct
+    public void init() {
+        graficar();
+    }
+
+    public void graficar() {
+        this.lineLocalesCircuitos = graficaCircuitos();
+
+    }
+
+    private void reinit() {
+        this.init();
+    }
+
+    public ControladorPermisosMunicipioCircuito() {
+        this.reinit();
+
+    }
+    
+    private CartesianChartModel graficaCircuitos() {
+        CartesianChartModel model = new CartesianChartModel();
+        try {
+
+            lstLocalesCircuito = FLocales.ObtenerDatosCircuito();
+            ChartSeries Si = new ChartSeries();
+            Si.setLabel("Si");
+            for (int i = 0; i < lstLocalesCircuito.size(); i++) {
+                lstLocalesDadoCircuitoSi = FLocales.ObtenerDatosDadoPermisoMunicipioCircuito("Si", lstLocalesCircuito.get(i).getCircuito());
+                Si.set(lstLocalesCircuito.get(i).getCircuito(), lstLocalesDadoCircuitoSi.size());
+            }
+
+            ChartSeries No = new ChartSeries();
+            No.setLabel("No");
+            for (int i = 0; i < lstLocalesCircuito.size(); i++) {
+                lstLocalesDadoCircuitoNo = FLocales.ObtenerDatosDadoPermisoMunicipioCircuito("No", lstLocalesCircuito.get(i).getCircuito());
+                No.set(lstLocalesCircuito.get(i).getCircuito(), lstLocalesDadoCircuitoNo.size());
+            }
+
+            ChartSeries NoPresenta = new ChartSeries();
+            NoPresenta.setLabel("No Presenta");
+            for (int i = 0; i < lstLocalesCircuito.size(); i++) {
+                lstLocalesDadoCircuitoNoPresenta = FLocales.ObtenerDatosDadoPermisoMunicipioCircuito("No Presenta", lstLocalesCircuito.get(i).getCircuito());
+                NoPresenta.set(lstLocalesCircuito.get(i).getCircuito(), lstLocalesDadoCircuitoNoPresenta.size());
+            }
+
+            model.addSeries(Si);
+            model.addSeries(No);
+            model.addSeries(NoPresenta);
+
+        } catch (Exception e) {
+            Util.addErrorMessage(e, "Error");
+        }
+        return model;
+
+    }
+
+}
