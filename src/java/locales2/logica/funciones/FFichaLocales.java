c0923ea450984745c770c5c228685dbcb4b72980
oscunach <oscunach@oscunach-PC>
2016-05-04 12:29:56 -0500

    Funciones Locales

--- src/java/locales2/logica/funciones/FFichaLocales.java
+++ src/java/locales2/logica/funciones/FFichaLocales.java
@@ -1,7 +1,78 @@
-
 package locales2.logica.funciones;
 
+import accesodatos.ConjuntoResultado;
+import java.util.ArrayList;
+import locales2.logica.clases.*;
+import vif.logica.clases.*;
+import vif.logica.funciones.*;
 
 public class FFichaLocales {
+
+//    public static ArrayList<FichaLocales> llenarDatosLocal(ConjuntoResultado rs) throws Exception {
+//        ArrayList<FichaLocales> lst = new ArrayList<FichaLocales>();
+//        FichaLocales fichaLocales = null;
+//        try {
+//            while (rs.next()) {
+//                fichaLocales = new FichaLocales(
+//                        rs.getInt("id_ficha_locales"),
+//                        rs.getString("parroquia"),
+//                        rs.getString("barrio"),
+//                        FCircuito.ObtenerCircuitoDadoId(rs.getInt("id_circuito")),
+//                        FSubcircuito.ObtenerSubcircuitoDadoId(rs.getInt("id_subcircuito")),
+//                        rs.getString("estado_local"),
+//                        rs.getString("razon_social"),
+//                        rs.getInt("categoria"),
+//                        rs.getString("tipo_establecimiento"),
+//                        rs.getString("direccion_establecimiento"),
+//                        rs.getString("nombre_propietario"),
+//                        rs.getString("cedula_propietario"),
+//                        rs.getString("telefono_propietario"),
+//                        rs.getDouble("x_local"),
+//                        rs.getDouble("y_local"),
+//                        rs.getString("permiso_municipio"),
+//                        rs.getString("permiso_bomberos"),
+//                        rs.getString("permiso_mds"),
+//                        rs.getString("permiso_mi"),
+//                        rs.getString("permiso_lt"),
+//                        rs.getString("sri"),
+//                        rs.getString("hora_inicio_diurno"),
+//                        rs.getString("hora_fin_diurno"),
+//                        rs.getString("hora_inicio_nocturno"),
+//                        rs.getString("hora_fin_nocturno"),
+//                        rs.getString("observaciones_hora"),
+//                        rs.getString("terrenos_valdios"),
+//                        rs.getString("espacios_abiertos"),
+//                        rs.getString("especificaciones_entorno"),
+//                        rs.getString("visualizacion"),
+//                        rs.getString("iluminacion"),
+//                        rs.getString("entrada_salida_emergencia"),
+//                        rs.getString("escondites"),
+//                        rs.getString("limpieza"),
+//                        rs.getString("senializado"),
+//                        rs.getString("extintores"),
+//                        rs.getString("observaciones_sistema_emergencia"),
+//                        rs.getString("sistema_seguridad"),
+//                        rs.getString("tipo_sistema_seguridad"),
+//                        rs.getString("observaciones_sistema_seguridad"),
+//                        rs.getInt("distancia_local_centro_educativo"),
+//                        rs.getString("direccion"),
+//                        rs.getDouble("x_centro_educativo"),
+//                        rs.getDouble("y_centro_educativo"),
+//                        rs.getString("nombre_encuestada"),
+//                        rs.getString("cedula_encuestada"),
+//                        rs.getString("telefono_encuestada"),
+//                        rs.getString("sugerencia_encuestada"),
+//                        rs.getString("observaciones_generales")
+//                );
+//                lst.add(fichaLocales);
+//            }
+//        } catch (Exception e) {
+//            lst.clear();
+//            throw e;
+//        }
+//        return lst;
+//    }
+    
+    
     
 }
