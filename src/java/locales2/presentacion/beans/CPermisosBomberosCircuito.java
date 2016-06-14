d530c2d245c0ab77bb54b726fbf5444be89751dc
Jhony J. Marcatoma <johnnysmarcatoma@gmail.com>
2016-06-14 11:38:50 -0500

    Jhony Marcatoma funcion ObtenerDatosDadoPermisoBomberosCircuito

--- src/java/locales2/presentacion/beans/CPermisosBomberosCircuito.java
+++ src/java/locales2/presentacion/beans/CPermisosBomberosCircuito.java
@@ -11,6 +11,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import locales2.logica.clases.FichaLocales;
 import locales2.logica.funciones.FFichaLocales;
+import locales2.logica.clases.Circuito;
 import org.primefaces.model.chart.CartesianChartModel;
 import org.primefaces.model.chart.ChartSeries;
 import recursos.Util;
